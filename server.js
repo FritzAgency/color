a2a37e206a0181039b62452d217cd82e8974603d
Adetona <adetonaabiodun12@gmail.com>
2018-04-26 14:50:24 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+ // res.write('Hello World!'); //write a response to the client
+ console.log('Hi Lazy Dev'); 
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
